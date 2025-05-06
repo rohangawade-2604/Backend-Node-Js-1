e5c799b5523049cd22bdbcb56ec287c22a80dc8c
rohangawade-2604 <rohangawade2604@gmail.com>
2025-05-06 17:32:15 +0530

    nothing done

---
+++ Day-1/index.js
@@ -0,0 +1,8 @@
+const OS = require("os");
+
+console.log(OS.EOL);
+console.log(OS.version())
+console.log(OS.availableParallelism());
+console.log(OS.arch());
+console.log(OS.constants);
+console.log(OS.cpus());
