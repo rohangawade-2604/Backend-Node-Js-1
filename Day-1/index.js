60e717236caa8995c3baa084043fba695fe12cf7
rohangawade-2604 <rohangawade2604@gmail.com>
2025-05-06 17:44:43 +0530

    done with it

--- Day-1/index.js
+++ Day-1/index.js
@@ -1,8 +1,13 @@
 const OS = require("os");
 
-console.log(OS.EOL);
+// console.log(OS.EOL);
 // console.log(OS.version())
 // console.log(OS.availableParallelism());
 // console.log(OS.arch());
 // console.log(OS.constants);
 // console.log(OS.cpus());
+// console.log(OS.endianness())
+// console.log(OS.devNull);
+// console.log(OS.freemem());
+// console.log(OS.getPriority(process.pid));
+// console.log(OS.homedir());
