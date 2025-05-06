f2d40502c478adb0b2cbe24347a8acf212bad52b
rohangawade-2604 <rohangawade2604@gmail.com>
2025-05-06 18:00:59 +0530

    noting done with it

--- Day-1/index.js
+++ Day-1/index.js
@@ -1,13 +1,25 @@
 const OS = require("os");
 
-// console.log(OS.EOL);
-// console.log(OS.version())
-// console.log(OS.availableParallelism());
-// console.log(OS.arch());
-// console.log(OS.constants);
-// console.log(OS.cpus());
-// console.log(OS.endianness())
-// console.log(OS.devNull);
-// console.log(OS.freemem());
-// console.log(OS.getPriority(process.pid));
-// console.log(OS.homedir());
+console.log(OS.EOL);
+console.log(OS.version())
+console.log(OS.availableParallelism());
+console.log(OS.arch());
+console.log(OS.constants);
+console.log(OS.cpus());
+console.log(OS.endianness())
+console.log(OS.devNull);
+console.log(OS.freemem());
+console.log(OS.getPriority(process.pid));
+console.log(OS.homedir());
+console.log(OS.hostname());
+console.log(OS.loadavg());
+console.log(OS.machine());
+console.log(OS.networkInterfaces());
+console.log(OS.platform());
+console.log(OS.setPriority(process.pid, 10));
+console.log(OS.tmpdir());
+console.log(OS.totalmem());
+console.log(OS.type());
+console.log(OS.uptime());
+console.log(OS.userInfo());
+console.log(OS.version());
