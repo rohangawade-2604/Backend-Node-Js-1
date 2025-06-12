1b614245f141b44cf972c79e3a83ae18df69ce28
rohangawade-2604 <rohangawade2604@gmail.com>
2025-06-12 12:37:03 +0530

    just completeing the task

--- Day-6-Express/ExpressdD1.js
+++ Day-6-Express/ExpressdD1.js
@@ -7,13 +7,18 @@ const port = 9000;
 const app = express();
 
 app.use(express.json());
+
 app.use(express.text());
 
 app.get('/showData', (req,res) => {
-    const data = fs.readFileSync('student.txt', {encoding: 'utf-8'} )
+    const data = fs.readFileSync('stdnt.json', 'utf-8')
     res.send(data);
-    // res.send('Hello Rohan welcome to the Express Database');
 });
+
+app.delete('/remove' , (req , res) => {
+     fs.rm('stdnt.json' )
+    res.send("Data is been deleted");
+})
 
 app.post('/student', (req, res) => {
     fs.writeFileSync('stdnt.json', JSON.stringify(req.body) , 'utf-8');         // this is used to write and create the file in the database in thunderclient
