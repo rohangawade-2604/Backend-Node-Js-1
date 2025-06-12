970f7a2f4d18176351b1feda2271406f9d3ffa21
rohangawade-2604 <rohangawade2604@gmail.com>
2025-06-12 13:18:15 +0530

    nothing

---
+++ Day-6-Express/SimpleMiddleWare.js
@@ -0,0 +1,12 @@
+const express =require ('express');
+
+const port = 9800;
+
+const app = express();
+
+app.use((req ,res, next) => {
+    console.log('hello i am the middleware');
+    next();
+    console.log('hello i am rohan gawade next middleware')
+});
+
