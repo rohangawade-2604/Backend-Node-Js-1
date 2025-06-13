9488cc285965ef9e718a8a3fecbc398b6caf9093
rohangawade-2604 <rohangawade2604@gmail.com>
2025-06-13 12:07:20 +0530

    nothing

--- Day-6-Express/SimpleMiddleWare.js
+++ Day-6-Express/SimpleMiddleWare.js
@@ -9,4 +9,44 @@ app.use((req ,res, next) => {
     next();
     console.log('hello i am rohan gawade next middleware')
 });
-hfjdklahfjkdshafjkdsha
+
+
+// Middleware to log the request method and URL
+
+app.use((req, res,next) => {
+    let auth = true;                                                     // Simulating an authentication check                                    
+    if (auth) {                                                            
+        res.send('you are not authorized to view this page ')
+    }
+    else{
+        next();
+        console.log('Welcome rohan Gawade ')
+    }
+})
+
+
+
+app.get('/', (req, res) => {
+    console.log('Hello i am from the base route...');
+    res.send('welcome');
+})
+
+app.get('/contact', (req, res) => {
+    console.log('Hello i am from the contact...');
+    res.send('contact');
+})
+
+app.get('/about', (req, res) => {
+    console.log('Hello i am from the about...');
+    res.send('about');
+})
+
+app.get('/blogs', (req, res) => {
+    console.log('Hello i am from the blogs...');
+    res.send('blogs');
+})
+
+app.listen(port,()=> {
+    console.log('port is active ${port');
+});
+
