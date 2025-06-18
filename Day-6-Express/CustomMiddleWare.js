28b462d07f212eecf90cb8acb2a502d8bf0030a6
rohangawade-2604 <rohangawade2604@gmail.com>
2025-06-18 16:40:57 +0530

    nothing

--- Day-6-Express/CustomMiddleWare.js
+++ Day-6-Express/CustomMiddleWare.js
@@ -9,7 +9,11 @@ app.use(express.json());
 
 const watchman = (req, res, next) => {
     const startTime = Date.now();
+    if(!req.body) {
+        req.body = {};
+    }
     req.body.server = 'Rohan is web developer';
+ 
     next();
     const endTime = new Date().getTime();
     console.log(endTime - startTime);
@@ -24,5 +28,32 @@ app.use(watchman , logger);
 
 app.get('/' , (req , res) => {
     console.log('e');
+    console.log(req.body.server);
     res.send('Hello Rohan, welcome to express part');
 });
+
+app.post('/addStudent', (req, res) => {
+    console.log(req.body);
+    res.send('got the data...')
+})
+
+app.get('/contact', (req,res) => {
+    console.log('hello i am from contact page');
+    res.send('contact');
+})
+
+app.get('/about', (req,res) => {
+    console.log('hello i am from about page');
+    res.send('about');
+})
+
+app.get('/blogs', (req, res) => {
+    console.log('hello i am from blogs')
+    const data = fs.readFileSync('./simpleMiddleWare.js', 'utf-8');
+    res.send(data);
+})
+
+app.listen(port, () => {
+    console.log('server is running on port ${port}')
+})
+
---
+++ Day-6-Express/MiddleWare.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const fs = require('fs')
+const cors =require('cors')
+const port = 9000;
+
+const {StudnetRouter} = require("./Routes/Student.Routes");
+const {lectureRoute} = require("/Routes/Lecture.Routes");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.text());
+
+app.use("/student", StudnetRouter);
+
+
+app.use(
+    cors({
+        origin:"example:website",
+        methods:"GET",
+    })
+);
+
+app.use("/lecture", lectureRoute);
+
+app.get('/', (req, res) => {
+    res.send('Hello, welcome to the Express server!');
+});
+
+app.post('/addStudent', (req, res) => {
+    console.log(req.body);
+    res.send('got the data...');
+})
+
+app.get('/contact', (req, res) => {
+    console.log('hello i am from contact page');
+    res.send('contact');
+})
+
+app.get('/about', (req, res) => {
+    console.log('hello i am from about page');
+    res.send('about');
+})
+
+app.get('/blogs', (req, res) => {
+    console.log('hello i am from blogs');
+    const data = fs.readFileSync('./simpleMiddleWare.js', 'utf-8');
+    res.send(data);
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
