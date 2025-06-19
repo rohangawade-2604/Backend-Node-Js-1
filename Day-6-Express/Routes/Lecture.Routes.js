39aa956d3b7b9dd47534a5badd860846dda0a131
rohangawade-2604 <rohangawade2604@gmail.com>
2025-06-19 15:50:43 +0530

    nothuing

---
+++ Day-6-Express/Routes/Lecture.Routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const lectureRoute = express.Router();
+
+lectureRoute.get('/' , (req, res) => {
+    res.send('all students');
+})
+
+lectureRoute.get('/web13' , (req, res) => {
+    res.send('web 13 students')
+})
+
+lectureRoute.post('/addLecture', (req,res) => {
+    console.log(req.body);
+    res.send('ok the lecture data wasa added')
+});
+
+module.exports = {lectureRoute};
---
+++ Day-6-Express/Routes/student.Routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const studentRoute = express.Router();
+
+studentRoute.get('/' , (req, res) => {
+    res.send('all students');
+})
+
+studentRoute.get('/web20' , (req, res) => {
+    res.send('web 20 students')
+})
+
+studentRoute.post('/addLecture', (req,res) => {
+    console.log(req.body);
+    res.send('ok the lecture data wasa added')
+});
+
+module.exports = {studentRoute};
