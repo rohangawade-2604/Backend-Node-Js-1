11bcd435145ee9ef935e1532ad64d61d8bcf8497
rohangawade-2604 <rohangawade2604@gmail.com>
2025-06-23 18:02:12 +0530

    nothing

---
+++ MongoDB/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const port = 9000;
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+});
+
+app.get('/weather', (req,res) => {
+    const data = {
+        banglore:'winter',
+        mumbai: 'haay garmi',
+        delhi: 'barish',
+        ghar: 'chadar le ke so jao'
+    }
+    const city = req.query.city;
+    const weather = data[city];
+    res.send('today weather is ${city} os ${weather}');
+});
+
+app.get('/students/studentID', (req, res) => {
+    const ID = req.params.studentID;
+    res.send(`Here is the data of student with ID is ${ID}`)
+})
+
+app.post('/addData', (req, res) => {
+    console.log(req.body);
+    res.send('Data received successfully');
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
