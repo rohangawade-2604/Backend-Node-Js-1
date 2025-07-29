d5cb54c311651694ba0f923b4b2d1b0a8d809169
rohangawade-2604 <rohangawade2604@gmail.com>
2025-07-29 13:42:43 +0530

    code

--- SignUp/Frontend/src/App.jsx
+++ SignUp/Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 
 import './App.css'
 import { Login } from './Login_Compo/login' 
+import { SignUp } from './Login_Compo/SignUp'
 
 function App() {
   return (
     <>
+    {/* <SignUp/> */}
       <Login/>
     </>
   )
--- SignUp/Frontend/src/Login_Compo/login.jsx
+++ SignUp/Frontend/src/Login_Compo/login.jsx
@@ -1,39 +1,35 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import {SignUp} from './SignUp'
+import { SignUp } from './SignUp'
 
 export const Login = () => {
-  return (
-    <>
-      <div className="login_page">
-        <div className="content">
-            <h1 className='heading'>Login Users</h1>
-        </div>
-
-        <div className="form">
-            <form action="">
-                <div>
-                    <input type="text" name='Username' placeholder='Enter the Username' />
+    return (
+        <>
+            <div className="login_page">
+                <div className="content">
+                    <h1 className='heading'>Login Users</h1>
                 </div>
 
-                <div>
-                     <input type="password" name='password' placeholder='Enter the password' />
+                <div className="form">
+                    <form action="">
+                        <div>
+                            <input type="text" name='Username' placeholder='Enter the Username' />
+                        </div>
+
+                        <div>
+                            <input type="password" name='password' placeholder='Enter the password' />
+                        </div>
+
+                    <a href="/signup?name=&age=&email=&password=">Dont have an account</a>
+                        <div>
+                           
+                            <button type='submit' onChange={SignUp}>Submit</button>
+                        </div>
+
+                    </form>
                 </div>
-                
-            
-
-                <h3  onClick={SignUp}>Don't have an account</h3>
-
-               <div>
-                {/*      */}
-                    <button type='submit'>Submit</button>
-               </div>
-                
-            </form>
-        </div>
-      </div>
-    </>
-  )
+            </div>
+        </>
+    )
 }
 
 
