1bf918994584cd12fb6269045ee3ae89aaa56ea3
rohangawade-2604 <rohangawade2604@gmail.com>
2025-07-31 01:14:29 +0530

    code

--- Crud_Operation/Frontend/src/Pages/Signup.jsx
+++ Crud_Operation/Frontend/src/Pages/Signup.jsx
@@ -3,7 +3,37 @@ import React from 'react'
 export const Signup = () => {
   return (
     <>
-      
+      <div className="signUp">
+        <div className="form_2">
+          <form action="">
+
+            <div className="name">
+              <input type="text" name='name' placeholder="Name" />
+            </div>
+
+
+            <div className="age">
+              <input type="number" name='age' placeholder="Age" />
+            </div>
+
+            
+            <div className="Email">
+              <input type="email" name='email' placeholder="email" />
+            </div>
+
+            
+            <div className="password">
+              <input type="text" name='password' placeholder="password" />
+            </div>
+
+            
+            <div className="ConfirmPassword">
+              <input type="text" name='confirmpassword' placeholder="ConfirmPassword" />
+            </div>
+
+          </form>
+        </div>
+      </div>
     </>
   )
 }
