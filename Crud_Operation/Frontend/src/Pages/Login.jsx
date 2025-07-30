f810808fd9af1fccab59949f502e2765bdf55980
rohangawade-2604 <rohangawade2604@gmail.com>
2025-07-31 01:01:49 +0530

    code

--- Crud_Operation/Frontend/src/Pages/Login.jsx
+++ Crud_Operation/Frontend/src/Pages/Login.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 export const Login = () => {
   return (
     <>
-      
+      <div className="Login">
+        <form action="">
+            <div className="username">
+                <input type="text" name='username' placeholder="Username" />
+            </div>
+
+            <div className="password">
+                <input type="password" name='password' placeholder="Password" />
+            </div>
+        </form>
+      </div>
     </>
   )
 }
