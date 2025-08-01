e30ece83275371eeb52a76628c8771b6b4852f3e
rohangawade-2604 <rohangawade2604@gmail.com>
2025-08-01 13:48:23 +0530

    code

--- Crud_Operation/Frontend/src/App/store.js
+++ Crud_Operation/Frontend/src/App/store.js
@@ -1,6 +1,6 @@
 import {ConfigueStore} from '@reduxjs/toolkit'
 
-import signupFun from './Features/SignIn/SignUpReducer'   // taken different name of SignupReducer as signupFun from the feature folder
+import signupFun from '../Features/SignIn/SignUpReducer'   // taken different name of SignupReducer as signupFun from the feature folder
 
 export const store = ConfigueStore({
     reducer: {
