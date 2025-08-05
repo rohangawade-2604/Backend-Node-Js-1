70a865fa121fb4cc70e83bac45745c3a1fb9b387
rohangawade-2604 <rohangawade2604@gmail.com>
2025-08-05 14:29:15 +0530

    code

--- Day-12/Server.js
+++ Day-12/Server.js
@@ -15,3 +15,27 @@ app.get("/", (req, res) => {
     res.send("Hello Rohan Gawade");
 })
 
+app.post("/signup", async (req, res) => {
+    const {email , password, age , name} = req.body
+
+    const userPresent = await UserModule.findOne({email})
+    if(userPresent) res.send('Try login in , already exist')
+        try{
+            bcrypt.hash(password, 5, async function(err, difficultPass) {
+
+                //store hash in your password DB
+
+                const users = new UserModule({
+                    email,
+                    password: difficultPass,
+                    age, 
+                    name,
+                });
+                await users.save();
+                res.send("Signup Succesfull...");
+            })
+        } catch(err) {
+            console.log(err);
+            res.send("something went wrong! please try again leater...");
+        }
+})
