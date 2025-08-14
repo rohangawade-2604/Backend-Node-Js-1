9524e4b6202d3afa18f2af69b65513d915ec6dfc
rohangawade-2604 <rohangawade2604@gmail.com>
2025-08-14 12:46:16 +0530

    cd

--- Day-13_Crud/Frontend/src/Reducer/signin/SignupReducer.jsx
+++ Day-13_Crud/Frontend/src/Reducer/signin/SignupReducer.jsx
@@ -16,3 +16,24 @@ export const { signIns } = signUpReducer.actions;
 export default signUpReducer.reducer;
 
 
+export const signUpThunk = (formData) => async (dispatch) => {
+    try{
+        const response = await fetch("http://localhost:8500/signup", {
+            method: "POST",
+            body: JSON.stringify(formData),
+            headers: {
+                "content-Type": "application/json"
+            },
+        });
+
+        const result = await response.json();
+        console.log("ApI response", result);
+
+        if(result.success){
+            alert(result.message);
+        }
+    }
+    catch(err){
+            console.log("API error", err);
+        }
+};
