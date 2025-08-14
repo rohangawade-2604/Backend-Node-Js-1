3f1ea5ce58aa42f1b0c43c8337c3bafe8836dce5
rohangawade-2604 <rohangawade2604@gmail.com>
2025-08-14 12:33:59 +0530

    code

--- Day-13_Crud/Frontend/src/main.jsx
+++ Day-13_Crud/Frontend/src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowswerRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import {store} from './App/store'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowswerRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowswerRouter>
+
+
+
 )
