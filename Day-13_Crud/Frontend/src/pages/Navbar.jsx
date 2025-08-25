dc98f2c19aa837bb630345d3ed322674534f06ef
rohangawade-2604 <rohangawade2604@gmail.com>
2025-08-25 16:26:24 +0530

    code

--- Day-13_Crud/Frontend/src/pages/Navbar.jsx
+++ Day-13_Crud/Frontend/src/pages/Navbar.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Logo from '../assets/Logo.png'
 
 export const Navbar = () => {
   return (
     <>
-        <div className="container">
+        <div className="container flex justify-around gap-20 items-center border-2 ">
+
+          <div className="logo">
+            <img src={Logo} alt="" className='w-30 h-30'/>
+          </div>
+
+          <div className="navbars flex justify-around gap-20 text-[20px] font-bold">
             <Link to="/">Home</Link>
             <Link to="/login">Login</Link>
             <Link to="/signup">SignUp</Link>
             <Link to="/product">Product</Link>
+          </div>
+
+          <div className="search-bar flex gap-5">
+            <input type="text" placeholder='Search...' className='border-b-2'/>
+            <button>Search</button>
+          </div>
         </div>
     </>
   )
--- Day-13_Crud/Frontend/src/pages/SignUp.jsx
+++ Day-13_Crud/Frontend/src/pages/SignUp.jsx
@@ -1,82 +1,93 @@
-import React from 'react'   
+import React from 'react'
 import { useState } from 'react'
-import{ useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
-import { signUpThunk} from '../Reducer/SignupReducer';
+import { signUpThunk } from '../Reducer/SignupReducer';
 
 
 
 export const SignUp = () => {
 
-    const dispatchSignUp = useDispatch();
+  const dispatchSignUp = useDispatch();
 
-    const fields = [
-        {
-          name: 'username', type: 'text', placeholder:'Enter your Username', label:'Username' , required: true
-        },
-    
-        {
-          name: 'email', type: 'email', placeholder:'Enter your Email', label:'Email' , required: true
-        },
-    
-        {
-          name: 'password', type: 'password', placeholder:'Enter your Password', label:'Password' , required: true
-        },
-    
-        {
-          name: 'confirmPassword', type: 'password', placeholder:'Enter your Confirm Password', label:'Confirm Password' , required: true
-        }
-    ];
+  const fields = [
+    {
+      name: 'username', type: 'text', placeholder: 'Enter your Username', label: 'Username', required: true
+    },
 
-    const [form , setform] = useState(() => {
-        return fields.reduce((acc , field) => {
-            acc[field.name] = '';
-            return acc
-        }, {})
+    {
+      name: 'email', type: 'email', placeholder: 'Enter your Email', label: 'Email', required: true
+    },
+
+    {
+      name: 'password', type: 'password', placeholder: 'Enter your Password', label: 'Password', required: true
+    },
+
+    {
+      name: 'confirmPassword', type: 'password', placeholder: 'Enter your Confirm Password', label: 'Confirm Password', required: true
+    }
+  ];
+
+  const [form, setform] = useState(() => {
+    return fields.reduce((acc, field) => {
+      acc[field.name] = '';
+      return acc
+    }, {})
+  })
+  console.log('form: ', form);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatchSignUp(signUpThunk(form))
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setform((prev) => {
+      return {
+        ...prev,
+        [name]: value
+      }
     })
-    console.log('form: ', form);
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        dispatchSignUp(signUpThunk(form)) 
-    }
-
-    const handleChange =(e) => {
-        const {name, value} = e.target;
-        setform((prev) => {
-            return{
-                ...prev,
-                [name]: value
-            }
-        })
-    }
+  }
 
 
   return (
     <>
-      <form action="" onSubmit={handleSubmit}>
-            
-            {
-                fields?.map((el ,i) => {
-                    return(
-                        <div key={i}>
-                            <label htmlFor={el.name}>{el.label}</label>
+      <div className="form_part1  mt-10">
 
-                            <input type={el.type} 
-                            id={el.id}
-                            name={el.name} 
-                            placeholder={el.placeholder} 
-                            onChange={(e) =>handleChange(e)} 
-                            required={el.required} 
-                        />
-                        </div>
-                    )
-                })
-            }
-            <button type='submit'>Submit</button>
-      </form>
+        <h1 className='text-center text-[20px] font-bold'>Sign Up </h1>
+
+        <form action="" onSubmit={handleSubmit} >
+
+          {
+            fields?.map((el, i) => {
+              return (
+                <div key={i} className='flex flex-col gap-6 items-center  '>
+                  <label htmlFor={el.name}></label>
+
+                  <input type={el.type}
+                    id={el.id}
+                    name={el.name}
+                    placeholder={el.placeholder}
+                    onChange={(e) => handleChange(e)}
+                    required={el.required}
+                    autoComplete="off"
+
+                    className='border-1 w-[400px] h-[40px] rounded-md text-center'
+                  />
+                </div>
+              )
+            })
+          }
+          <div className="button_part text-center mt-8">
+            <button type='submit' className=' w-[400px] h-[40px] rounded-md bg-green-500 text-white transition-all hover:bg-green-600 hover:text-black '>Submit</button>
+          </div>
+        </form>
+      </div>
+
     </>
   )
 }
 
- 
+
