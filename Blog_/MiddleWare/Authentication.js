540f6f7dd7ed59572b179771124b9fd71dd7d831
rohangawade-2604 <rohangawade2604@gmail.com>
2025-10-03 15:39:37 +0530

    code

---
+++ Blog_/MiddleWare/Authentication.js
@@ -0,0 +1,10 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const Authentication = (req, res, next) => {
+
+    const VerifyToken = 
+}
+
+
+module.exports = { Authentication }
