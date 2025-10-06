93d095a379bf193fb8396d06393ef7ce541bdeb9
rohangawade-2604 <rohangawade2604@gmail.com>
2025-10-06 16:55:51 +0530

    rohan is added

--- Blog_App/Blog_/routes/Notes.routes.js
+++ Blog_App/Blog_/routes/Notes.routes.js
@@ -6,25 +6,26 @@ const notesRoutes = express.Router();
 
 // read
 notesRoutes.get('/', async (req, res) => {
-    try{
+    try {
         const dbNotes = await NotesModule.find();
         res.status(200).send(dbNotes);
     }
-    catch(err){
-        res.status(401).send({msg:"something went wrong", error: err})
+    catch (err) {
+        res.status(401).send({ msg: "something went wrong", error: err })
     }
 })
 
 // create
 notesRoutes.post('/create', async (req, res) => {
     const data = req.body;
+    let data2 = { ...req.user, ...data };
     console.log("🚀 ~ req:", req)
     console.log(data, "data is send note routes");
 
     try {
-        const newNotes = new NotesModule(data);
+        const newNotes = new NotesModule(data2);
         await newNotes.save();
-        res.status(201).send({ msg: "new notes created successfully", payload: data })
+        res.status(201).send({ msg: "new notes created successfully", payload: data2 })
     } catch (error) {
         console.log(error)
         res.status(401).send({ msg: "Something went wrong, please try again later ", error: error })
@@ -32,7 +33,7 @@ notesRoutes.post('/create', async (req, res) => {
 })
 
 // update
-notesRoutes.patch('/edit/:nodeIDs', async(req, res) => {
+notesRoutes.patch('/edit/:nodeIDs', async (req, res) => {
     const data = req.body;
 
     const nodeID = req.params.nodeIDs;
@@ -40,18 +41,18 @@ notesRoutes.patch('/edit/:nodeIDs', async(req, res) => {
     const UserchiID = req.user.userID    // called from authentication middleware //
     console.log(UserchiID, "user id in notes routes");
 
-    const notes = await NotesModule.findOne({ _id: nodeID})
+    const notes = await NotesModule.findOne({ _id: nodeID })
 
-    if (UserchiID !== notes.userID)  {
+    if (UserchiID !== notes.userID) {
 
         console.log("you are not authorized person in the website")
 
     } else {
-        
-        try {
-            let noteUpdates = await NotesModule.findByIdAndUpdate({ _id: nodeID }, data) 
 
-            res.send({ msg: `your task has been updated successfully ${noteUpdates} with it ID ${nodeID}`})
+        try {
+            let noteUpdates = await NotesModule.findByIdAndUpdate({ _id: nodeID }, data)
+
+            res.send({ msg: `your task has been updated successfully ${noteUpdates} with it ID ${nodeID}` })
         } catch (error) {
             console.log(error, "you found an error")
         }
@@ -60,29 +61,33 @@ notesRoutes.patch('/edit/:nodeIDs', async(req, res) => {
 })
 
 //delete
-notesRoutes.delete('/delete/:nodeIDs', async(req, res) => {
+notesRoutes.delete('/delete/:nodeIDs', async (req, res) => {
+
+    const data = req.body;
 
     const nodeID = req.params.nodeIDs;
 
     const UserchiID = req.user.userID    // called from authentication middleware //
     console.log(UserchiID, "user id in notes routes");
 
-    const notes = await NotesModule.findOne({_id: nodeID})
+    const notes = await NotesModule.findOne({ _id: nodeID })
 
-    if (UserchiID !== notes.userID)  {
+    if (UserchiID !== notes.userID) {
 
         console.log("you are not authorized person in the website")
 
     } else {
-        
-        try {
-            let noteDelete = await NotesModule.findbyIdAndDelete({ _id: nodeID }, data) 
 
-            res.send({ msg: `your task has been Deleted successfully ${noteDelete} with it ID ${nodeID}`})
+        try {
+            let noteDelete = await NotesModule.findByIdAndDelete({ _id: nodeID }, data)
+            console.log("🚀 ~ noteDelete:", noteDelete);
+            
+
+            res.send({ msg: `your task has been Deleted successfully with it ID ${nodeID}` })
         } catch (error) {
             console.log(error, "you found an error")
         }
-    }
+    }   
 
 })
 
