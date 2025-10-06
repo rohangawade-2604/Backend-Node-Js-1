4505b54ecb3369f9798d3655f352828140a071b3
rohangawade-2604 <rohangawade2604@gmail.com>
2025-10-06 13:47:18 +0530

    code

--- Blog_App/Frontend/src/API/Blog_Read.js
+++ Blog_App/Frontend/src/API/Blog_Read.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
-export const Blog_Read = async(url) => {
+export const Blog_Read = async(url, token) => {
     try{
-        const res = await axios.get(`${url}/note`)
+        const res = await axios.get(`${url}/note`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
         return res.data;
     }
     catch(err){
