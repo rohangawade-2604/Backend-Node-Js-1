8958922a58b8531225da2c4b48f98866c9ede13f
rohangawade-2604 <rohangawade2604@gmail.com>
2025-10-06 13:09:48 +0530

    code

--- Blog_App/Frontend/src/Pages/Login.jsx
+++ Blog_App/Frontend/src/Pages/Login.jsx
@@ -1,17 +1,38 @@
 import React from 'react'
+import { useState } from 'react';
+
+import { Login } from "../API/Login"
+
+const baseURL = import.meta.env.BASE_URL;
 
 
+export const LoginPage = () => {
 
-export const Login = () => {
+  const { email, setEmail } = useState(null);
+  const { password, setPassword } = useState(null);
+
+  const fetchData = async () => {
+    const final = await Login(baseURL, { email:email.trim(), password:password.trim()})
+    console.log(final, "final answer is executed...")
+  }
+
+  const handleSubmitChange = (e) => {
+    e.preventDefault();
+
+    if(email === "" && password === "")
+
+    fetchData()
+  }
+
   return (
-
     <>
-      <form action="">
+      <form action="" onSubmit={() => handleSubmitChange()}>
 
-       email: <input type="text" name='email' placeholder='Enter your Email' />
-       password: <input type="text" name='password' placeholder='Enter your password' />  
+        email: <input type="text" name='email' placeholder='Enter your Email' onChange={(e) => setEmail(e.target.value)}/>
+        
+        password: <input type="text" name='password' placeholder='Enter your password' onChange={(e) => setPassword(e.target.value)}/>
 
-       <button type='button'>Submit</button>     
+        <button type='button'>Submit</button>
       </form>
     </>
   )
--- Blog_App/Frontend/src/Pages/Signup.jsx
+++ Blog_App/Frontend/src/Pages/Signup.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const Signup = () => {
+import {Signup} from '../API/Signup'
+
+const baseURL = import.meta.env.BASE_URL
+
+export const SignupPage = () => {
+
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [name, setName] = useState(null);
+
+  const fetchData = async() => {
+    let final = await Signup(baseURL, { email:email.trim(), password:password.trim(), name:name.trim()})
+    console.log(final, "final execution is situated")
+  }
+ 
+  const handleSubmitChange = (e) => {
+    e.prevantDefault();
+
+    if(email === "" && password === "" && name === "") 
+      return;
+
+    fetchData();
+  }
   
   return (
     <>
-      <form action="">
+      <form action="" onSubmit={() => handleSubmitChange(event)}>
         
-        Name:<input type="text" name='name' placeholder='Enter your Name' />
-        email: <input type="text" name='email' placeholder='Enter your Email' />
-        password: <input type="text" name='password' placeholder='Enter your Password' />
+        Name:<input type="text" name='name' placeholder='Enter your Name'  onChange={(e) => setEmail(e.target.value)} />
 
+        email: <input type="text" name='email' placeholder='Enter your Email' onChange={(e) => setPassword(e.target.value)} />
+
+        password: <input type="text" name='password' placeholder='Enter your Password' onChange={(e) => setName(e.target.value)} />
+()
          <button type='button'>Submit</button>     
       </form>
     </>
