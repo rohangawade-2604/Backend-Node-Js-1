e6bf52c36049bb043a7a562a708adc3bac4049aa
rohangawade-2604 <rohangawade2604@gmail.com>
2025-10-28 15:58:59 +0530

    code

--- Blog_App/Frontend/src/Routes/AllRoutes.jsx
+++ Blog_App/Frontend/src/Routes/AllRoutes.jsx
@@ -23,7 +23,6 @@ export const AllRoutes = () => {
         <Route path='/contact' element={<Contact />}></Route>
         <Route path='/login' element={<LoginPage />}></Route>
         <Route path='/blog-read' element={<Blog_ReadPage />}></Route>
-        <Route path='/blog-read' element={<Blog_ReadPage />}></Route>
    
       </Routes>
     </>
