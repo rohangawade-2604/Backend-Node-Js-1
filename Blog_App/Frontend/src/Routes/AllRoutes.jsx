bbb4891b2a7aefe4e803a8acdf7843a1d82a5134
rohangawade-2604 <rohangawade2604@gmail.com>
2025-10-28 15:58:44 +0530

    code

--- Blog_App/Frontend/src/Routes/AllRoutes.jsx
+++ Blog_App/Frontend/src/Routes/AllRoutes.jsx
@@ -16,7 +16,6 @@ export const AllRoutes = () => {
         {/* connected to the login page for user should go throught the signup page */}
         <Route path='/signup' element={<SignupPage />}></Route>
 
-
         <Route path='/' element={<Home />}></Route>
         <Route path='/categories' element={<Categories />}></Route>
         <Route path='/about' element={<About />}></Route>
@@ -24,10 +23,8 @@ export const AllRoutes = () => {
         <Route path='/contact' element={<Contact />}></Route>
         <Route path='/login' element={<LoginPage />}></Route>
         <Route path='/blog-read' element={<Blog_ReadPage />}></Route>
-       
-       
-
-
+        <Route path='/blog-read' element={<Blog_ReadPage />}></Route>
+   
       </Routes>
     </>
   )
